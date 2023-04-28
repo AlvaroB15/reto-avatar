9ddc5e51d22d72a7f0b33dbb7cbd114a091c3eee
Alvaro Begazo Carhuayo <alvaro15unmsm@gmail.com>
2023-04-28 16:59:47 -0500

    remove imports

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,35 +1,22 @@
 import {NgModule} from '@angular/core';
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
-import {MatListModule} from "@angular/material/list";
 import {MatCardModule} from "@angular/material/card";
 import {MatDialogModule} from "@angular/material/dialog";
-import {MatExpansionModule} from "@angular/material/expansion";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
-import {MatSelectModule} from "@angular/material/select";
 import {MatChipsModule} from '@angular/material/chips';
 import {MatTabsModule} from '@angular/material/tabs';
-
 
 @NgModule({
     declarations: [],
     exports: [
         MatSidenavModule,
-        MatToolbarModule,
-        MatIconModule,
         MatButtonModule,
-        MatListModule,
         MatCardModule,
         MatDialogModule,
-        MatExpansionModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatSelectModule,
         MatChipsModule,
-        MatTabsModule
+        MatTabsModule,
+        MatToolbarModule
     ]
 })
 export class MaterialModule {
